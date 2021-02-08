bbb6add373a892abee5f83e1c266f8652aad10a0
bigidea-kz <44386084+bigidea-kz@users.noreply.github.com>
2021-02-09 03:41:44 +0600

    Update resolveProductionUrl.js

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,5 +1,5 @@
 const previewSecret = 'frservice2020'
-const projectUrl = 'http://localhost:3000'
+const projectUrl = 'http://www.fr-s.de'
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
